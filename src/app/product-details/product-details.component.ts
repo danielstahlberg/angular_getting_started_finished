b1e784056364d3f8af0fd6f60a5ccebe8bc5d6b9
danielstahlberg <daniel.stahlberg@syspartner.se>
2022-02-01 11:08:34 +0100

    Added "auto"-generated product detail pages

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -8,10 +8,16 @@ import { Product, products } from '../products';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+  
+  product: Product | undefined;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    this.product = products.find(product => product.id === productIdFromRoute);
   }
 
 }
